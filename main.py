12a875fe1f9bcb6ff16ff48c42481a11cf95532a
Eoin Costello <eoinc134@gmail.com>
2026-06-13 20:24:04 +0200

    Update logger

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from routes.habits import router
 app = FastAPI()
 app.include_router(router)
 logger = logging.getLogger(__name__)
-logger.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 @app.exception_handler(Exception)
 def exception_handler(request, exc):
